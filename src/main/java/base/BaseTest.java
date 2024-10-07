c1537428d43d48e03c704082bec45278cae167ab
Bohdan Latkovskyi <bogdanlotuk@gmail.com>
2024-10-07 16:13:01 -0600

    Chromedriver path has been added

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,7 +2,10 @@ package base;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -10,10 +13,13 @@ public class BaseTest {
     public Constants constants = new Constants();
     @BeforeClass
     public void BeforeClass(){
-        WebDriverManager.chromedriver().setup();
-//        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-        Configuration.browserSize = "1366x768";
+//        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//        Configuration.browserSize = "1366x768";
 //        System.out.println("BeforeClass started");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverRunner.setWebDriver(driver);
     }
 
     @AfterClass
